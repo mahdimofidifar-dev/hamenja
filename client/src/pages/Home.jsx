f38d5601f849be5b50743358870cd690ee859bed
mahdimofidifar-dev <mahdi2008.mofidifar@gmail.com>
2026-09-10 23:20:16 +0330

    try

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import CategoryBox from "@/components/vendor/CategoryBox";
 import SearchBox from "@/components/forms/SearchBox";
 import { Coffee, Scissors, SquareOff } from "lucide-react";
 // import { CarouselY } from "@/components/ui/CarouselY";
-import { getUsers } from "../api/users.js";
+import { getUsers } from "../API/users.js";
 import { useEffect, useState } from "react";
 
 export default function Home() {
